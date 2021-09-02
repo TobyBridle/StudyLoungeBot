6643e71270ca328c892451e366f8d6480a3316dd
Toby Bridle <tobybridle@Tobys-Air.lan>
2021-09-02 14:55:11 +0100

    Fixed issues from code review

--- scripts/commands.js
+++ scripts/commands.js
@@ -2,27 +2,29 @@ require('dotenv').config()
 const { BOT_PREFIX } = process.env
 const Discord = require('discord.js')
 
+const CHANNELS = {
+    'stopStudyChannel': '882783531531653210'
+}
+
 const changeEnv = require('../server.js')
 
 exports.study = async (message, args, guildRoles) => {
     if(!typeof([args][0], 'int') && args) return message.reply(`To enter study mode:\n${BOT_PREFIX}study`)
-    args
-    ?
-    message.author.send(`You\'re now studying for ${args} minutes`)
-    :
+    
     message.author.send('You\'re now studying!')
 
-    message.guild.channels.cache.get('882783531531653210').send(`Welcome ${message.author} to Study Mode. Type \`${BOT_PREFIX}stop\` to end the session.`)
+    message.guild.channels.cache.get(CHANNELS.stopStudyChannel).send(`Welcome ${message.author} to Study Mode. Type \`${BOT_PREFIX}stop\` to end the session.`)
     guildRoles.forEach(r => {
-        r.role.name === 'Studying' ? message.member.roles.add(r.role.id) : ''
+        if(r.role.name === 'Studying') message.member.roles.add(r.role.id)
     })
 
     await message.delete()
 }
 
 exports.stop = async (message, args, guildRoles) => {
+    if (message.channelId !== CHANNELS.stopStudyChannel) return 0
     guildRoles.forEach(r => {
-        r.role.name === 'Studying' ? message.member.roles.remove(r.role.id) : ''
+        if(r.role.name === 'Studying') message.member.roles.remove(r.role.id)
     })
 
     await message.author.send('Study Mode Ended!')
@@ -32,5 +34,5 @@ exports.ping = (message, args) => {
 }
 
 exports.set = (message, args, guildRoles) => {
-    changeEnv('BOT_PREFIX', '!')
+    changeEnv('BOT_PREFIX', args[0])
 }
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 require('dotenv').config()
 const { TOKEN, BOT_PREFIX } = process.env
 const fs = require('fs')
-const { exec } = require("child_process");
 
 const { Client, Intents, Collection, Role } = require('discord.js')
+
 const botIntentList = new Intents()
 botIntentList.add(
     Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.DIRECT_MESSAGES
     )
+
 const client = new Client({ intents: botIntentList })
 const commands = require('./scripts/commands.js');
 const Roles = {}
+
 const changeEnvInfo = (row, data) => {
     envData = fs.readFileSync('./.env').toString().trim().split('\n')
     tmp = ''
@@ -24,7 +26,6 @@ const changeEnvInfo = (row, data) => {
 
     fs.writeFileSync('./.env', tmp.toString())
     console.log('Changed a value in .env')
-    process.exit(1) // Using Forever module the server will restart
 }
 
 client.once('ready', (c) => {
@@ -46,9 +47,8 @@ client.on('messageCreate', (message) => {
     if (message.content.startsWith(BOT_PREFIX))
     {
         // Get Command
-     
         let [command, args]= message.content.slice(1, message.content.length).split(' ')
-        commands[command] ? (commands[command](message, args, Roles[message.member.guild.id])) : ''
+        if(commands[command]) (commands[command](message, args, Roles[message.member.guild.id]))
     }
 })
 
